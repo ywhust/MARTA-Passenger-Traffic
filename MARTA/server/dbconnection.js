cd3fdf62e17a671bc51451b6fe6566fb6ed0b99d
Yang Wu <ywu613@gmail.com>
2017-12-14 23:11:33 -0500

    change folder structure

--- MARTA/server/dbconnection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: 'academic-mysql.cc.gatech.edu',
-    user: 'cs4400_Group_111',
-    password: 'u1kjn118',
-    database: 'cs4400_Group_111'
-});
-
-module.exports = connection;
